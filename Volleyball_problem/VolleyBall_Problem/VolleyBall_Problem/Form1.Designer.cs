16ad85136aefd3d89244a53ed1d6e8cc344b17d3
Michael Lim <Michael Lim>
2016-11-26 21:04:27 +0700

    volleyball_Problem

---
+++ Volleyball_problem/VolleyBall_Problem/VolleyBall_Problem/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace VolleyBall_Problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnhitung = new System.Windows.Forms.Button();
+            this.txthasil = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(65, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Volley Ball Match Problem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(40, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score A";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(186, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Score B";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(26, 112);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(75, 20);
+            this.txt1.TabIndex = 5;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(169, 112);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(75, 20);
+            this.txt2.TabIndex = 6;
+            // 
+            // btnhitung
+            // 
+            this.btnhitung.BackColor = System.Drawing.Color.White;
+            this.btnhitung.ForeColor = System.Drawing.Color.Black;
+            this.btnhitung.Location = new System.Drawing.Point(96, 158);
+            this.btnhitung.Name = "btnhitung";
+            this.btnhitung.Size = new System.Drawing.Size(75, 23);
+            this.btnhitung.TabIndex = 7;
+            this.btnhitung.Text = "Result";
+            this.btnhitung.UseVisualStyleBackColor = false;
+            this.btnhitung.Click += new System.EventHandler(this.Btnresult_Click);
+            // 
+            // txthasil
+            // 
+            this.txthasil.BackColor = System.Drawing.Color.White;
+            this.txthasil.ForeColor = System.Drawing.Color.Black;
+            this.txthasil.Location = new System.Drawing.Point(84, 214);
+            this.txthasil.Name = "txthasil";
+            this.txthasil.Size = new System.Drawing.Size(100, 20);
+            this.txthasil.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txthasil);
+            this.Controls.Add(this.btnhitung);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnhitung;
+        private System.Windows.Forms.TextBox txthasil;
+    }
+}
+
---
+++ Volleyball_problem/VolleyBall_Problem/VolleyBall_Problem/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolleyBall_Problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public const int Modular = 1000000007;
+
+        static bool Valid(int a, int b)
+        {
+            if (a < 25) return false;
+            if (a == 25) return b <= 23;
+            return b == (a - 2);
+        }
+
+        public static void Swap<T>(ref T left, ref T right)
+        {
+            T temp;
+            temp = left;
+            left = right;
+            right = temp;
+        }
+
+        public static int nCr(int n, int r, int mod)
+        {
+            if (n < r) Swap(ref n, ref r);
+            if (n < 1 || r < 1) return 1;
+            var row = new int[r];
+            for (var i = 0; i < r; row[i++] = 1) { }
+
+            for (int i = 2; i < r; ++i)
+            {
+                for (int j = i - 1; j > 0; --j)
+                {
+                    row[j] = (row[j] + row[j - 1]) % mod;
+                }
+            }
+            --r;
+            int sum = row[r];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = r; j > 0; --j)
+                {
+                    row[j] = (row[j] + row[j - 1]) % mod;
+                }
+                sum = (sum + row[r]) % mod;
+            }
+            return sum;
+        }
+
+        private static int pow(int baseNo, int exponent, int mod)
+        {
+            if (exponent < 2)
+            {
+                return (exponent < 1) ? 1 : baseNo;
+            }
+            long product = pow(baseNo, exponent >> 1, mod);
+            product = (product * product) % mod;
+            return ((exponent & 1) == 1) ? (int)((product * baseNo) % mod) : (int)(product);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnresult_Click(object sender, EventArgs e)
+        {
+            var a = Convert.ToInt32(txt1.Text);
+            var b = Convert.ToInt32(txt2.Text);
+            int result;
+
+            if (a < b) Swap(ref a, ref b);
+            if (!Valid(a, b))
+            {
+                result = 0;
+            }
+            else
+            {
+                if (a > 25)
+                {
+                    result = nCr(24, 24, Modular);
+                    result = (int)((((long)result) * pow(2, b - 24, Modular)) % Modular);
+                }
+                else
+                {
+                    result = nCr(a - 1, b, Modular);
+                }
+            }
+            txthasil.Text = result.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
